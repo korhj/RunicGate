305cdce4a718e6961267b157169b3444f094e017
Jyri Korhonen <jyri.korhonen@outlook.com>
2025-05-07 02:01:53 +0300

    Removed overlayTiles

--- Assets/Scripts/MapManager.cs
+++ Assets/Scripts/MapManager.cs
@@ -9,16 +9,6 @@ public class MapManager : MonoBehaviour
     public static MapManager Instance { get; private set; }
     public bool IsReady { get; private set; } = false;
 
-    /*
-    [SerializeField]
-    private OverlayTile overlayTilePrefab;
-
-    [SerializeField]
-    private GameObject overlayContainer;
-
-    private Dictionary<Vector2Int, OverlayTile> map;
-    */
-
     [SerializeField]
     private Tilemap tilemap;
     public Tilemap Tilemap => tilemap;
@@ -40,69 +30,14 @@ public class MapManager : MonoBehaviour
             Debug.LogError("Tilemap not found.");
         }
 
-        //map = new Dictionary<Vector2Int, OverlayTile>();
         runicGates = new List<(GameObject, Vector3Int)>();
     }
 
     private void Start()
     {
-        //GenerateOverlayTiles();
         IsReady = true;
     }
 
-    /*
-    private void GenerateOverlayTiles()
-    {
-        BoundsInt bounds = tilemap.cellBounds;
-
-        for (int z = bounds.max.z; z >= bounds.min.z; z--)
-        {
-            for (int y = bounds.max.y; y >= bounds.min.y; y--)
-            {
-                for (int x = bounds.max.x; x >= bounds.min.x; x--)
-                {
-                    var tileLocation = new Vector3Int(x, y, z);
-                    var tileKey = new Vector2Int(x, y);
-                    if (tilemap.HasTile(tileLocation) && !map.ContainsKey(tileKey))
-                    {
-                        var overlayTile = Instantiate(
-                            overlayTilePrefab,
-                            overlayContainer.transform
-                        );
-                        var cellWorldPosition = tilemap.GetCellCenterWorld(tileLocation);
-
-                        overlayTile.transform.position = new Vector3(
-                            cellWorldPosition.x,
-                            cellWorldPosition.y,
-                            cellWorldPosition.z + 1
-                        );
-
-                        overlayTile.GetComponent<SpriteRenderer>().sortingOrder = tilemap
-                            .GetComponent<TilemapRenderer>()
-                            .sortingOrder;
-
-                        map.Add(tileKey, overlayTile);
-                    }
-                }
-            }
-        }
-    }
-
-    void Update() { }
-
-    public OverlayTile GetOverlayTileFromTileLocation(Vector2Int tileLocation)
-    {
-        if (map.ContainsKey(tileLocation))
-        {
-            return map[tileLocation];
-        }
-        else
-        {
-            Debug.LogWarning($"Tile at {tileLocation} not found.");
-            return null;
-        }
-    }
-    */
     public Vector3Int? GetTilePosFromTileCoordinates(Vector2Int tileCoordinates)
     {
         BoundsInt bounds = tilemap.cellBounds;
@@ -136,7 +71,7 @@ public class MapManager : MonoBehaviour
                 return;
             }
         }
-
+        Debug.Log($"PlayerInteract after loop {interactedTileCoordinates}");
         if (tilemap.HasTile(interactedTileCoordinates))
         {
             AddRunicGate(interactedTileCoordinates);
