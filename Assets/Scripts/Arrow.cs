74c12cc4fcc3a1110f5ec4d33f0ec623ebd21e1b
Jyri Korhonen <jyri.korhonen@outlook.com>
2025-06-20 05:32:35 +0300

    Fix bug in arrow destruction
    Arrows are destroyed only on collision or timer

--- Assets/Scripts/Arrow.cs
+++ Assets/Scripts/Arrow.cs
@@ -6,16 +6,6 @@ public class Arrow : MonoBehaviour
     Rigidbody2D rb2D;
     int arrowDamage = 0;
 
-    private void Update()
-    {
-        Vector3Int tilePos = MapManager.Instance.WorldToTile(transform.position);
-        Vector3Int? tileAtPos = MapManager.Instance.FindWalkableTileAt(tilePos, 0, 2);
-        if (!tileAtPos.HasValue)
-        {
-            Destroy(gameObject);
-        }
-    }
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.TryGetComponent<IObstacle>(out _))
